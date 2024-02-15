b5c02727a4e24161ba0fead0e312594331423cdf
Spencer Sliffe <spenceronw@ku.edu>
2024-02-15 09:18:23 -0600

    pushing this because i am going to restructure KobraView

---
+++ Kobra/CustomElements/Cells/ParticipantCell.swift
@@ -0,0 +1,8 @@
+//
+//  ParticipantCell.swift
+//  Kobra
+//
+//  Created by Spencer Sliffe on 2/7/24.
+//
+
+import Foundation
---
+++ Kobra/Kobra/Views/ParticipantView.swift
@@ -0,0 +1,8 @@
+//
+//  ParticipantView.swift
+//  Kobra
+//
+//  Created by Spencer Sliffe on 2/7/24.
+//
+
+import Foundation
